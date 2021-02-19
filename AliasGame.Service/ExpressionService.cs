77635cf5466e56b362bb6d52b86b432ae50d28cf
explosion204 <dzmitriy20magic@gmail.com>
2021-02-19 15:44:29 +0300

    api: services layer placeholder

---
+++ AliasGame.Service/ExpressionService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using AliasGame.Domain;
+using AliasGame.Domain.Models;
+
+namespace AliasGame.Service
+{
+    public class ExpressionService
+    {
+        private readonly IRepository<Expression> _expressionRepository;
+
+        public ExpressionService(IRepository<Expression> expressionRepository)
+        {
+            _expressionRepository = expressionRepository;
+        }
+
+        public List<Expression> GetExpressions(int count)
+        {
+            var allExpressions = _expressionRepository.GetAllEntities();
+            var targetExpressions = new List<Expression>();
+            var random = new Random(DateTime.Now.Second);
+
+            for (var i = 0; i < count; i++)
+            {
+                var expression = allExpressions[random.Next(count)];
+                targetExpressions.Add(expression);
+            }
+
+            return targetExpressions;
+        }
+    }
+}
---
+++ AliasGame.Service/SessionService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AliasGame.Domain;
+using AliasGame.Domain.Models;
+
+namespace AliasGame.Service
+{
+    public class SessionService
+    {
+        private readonly IRepository<Session> _sessionRepository;
+
+        public SessionService(IRepository<Session> sessionRepository)
+        {
+            _sessionRepository = sessionRepository;
+        }
+
+        public Guid CreateSession(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool JoinSession(Guid userId, Guid sessionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
