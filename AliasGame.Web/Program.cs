f4ae9dbc2a107e017ede428b8c66c93c6e5fdf4c
explosion204 <dzmitriy20magic@gmail.com>
2021-04-26 12:07:05 +0300

    kestrel compatibility

--- AliasGame.Web/Program.cs
+++ AliasGame.Web/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,14 @@ namespace AliasGame
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel()
+                        .ConfigureKestrel(opt =>
+                        {
+                            opt.Listen(IPAddress.Loopback, 5001);
+                        });
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
